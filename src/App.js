7e436fb851801a7cdeb09cccb1b6510c0949523f
GamithaGimhana <gamitha.gimhana99@gmail.com>
2023-11-05 12:57:55 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -1,52 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import Vehicle from './Vehicle';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Product from './pages/Product';
 
 function App() {
 
-  const [username, setUsername] = useState(null);
-  const [counter, setCounter] = useState(0);
-
-  const handleLogin = () =>{
-    //user login process
-    setUsername("Gamitha");
-  }
-
-  const increaseCounter = () => {
-    setCounter(counter + 1);
-  }
-
-  const decreaseCounter = () => {
-    setCounter(counter - 1);
-  }
-
-  return (
+  return(
     <div className="App">
-
-      <div>
-        <h1>Hi {username}</h1>
-        <button onClick={handleLogin}>Login</button>
-      </div>
-
-      <div>
-        <h1>Counter: {counter}</h1>
-        <button onClick={increaseCounter}>Increase</button>
-        <button onClick={decreaseCounter}>Decrease</button>
-      </div>
-
-
-      <SampleComponent title="Passing title from Parent" />
-      <Vehicle model="Toyota Corolla" description="A family car" />
-      <Vehicle model="Landcruiser" description="A SUV" />
-      <Vehicle model="Nissan Sunny" />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
-
-const SampleComponent = (props) => {
-  return (
-    <h1>{props.title}</h1>
-  )
 }
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+const Home = () => {
+    return(
+        <div>
+            <h1>Home</h1>
+            <Lin to="/product">Products</Lin>
+        </div>
+    )
+}
+
+export default Home;
