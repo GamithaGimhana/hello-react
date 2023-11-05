f06228b4faf68adfbcd8c6a987360e58398eaa27
GamithaGimhana <gamitha.gimhana99@gmail.com>
2023-11-05 13:24:07 +0530

    setPages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Product from './pages/Product';
 
-function App() {
+function App () {
 
   return(
     <div className="App">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,32 @@
 import { Link } from "react-router-dom";
+
 const Home = () => {
+
+    const getProducts = () => {
+        fetch("http://localhost:8080/products")
+            .then((response) => {
+                return response.json();
+            }).then((data) => {
+                setProducts(data);
+
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
+
     return(
         <div>
             <h1>Home</h1>
-            <Lin to="/product">Products</Lin>
+            <Link to="/product">Products</Link>
+            <button onClick={getProducts}>Load Products</button>
+
+            {products &&
+                <ul>
+                    {products.map((product) => ( 
+                    <Link to={`/products/${product.id}`}>{product.name}</Link>    
+                    ))}
+                </ul>
+            }
         </div>
     )
 }
