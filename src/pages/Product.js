38193e5387dce549e68ad734a14bc5865f431aec
GamithaGimhana <gamitha.gimhana99@gmail.com>
2023-11-07 20:26:57 +0530

    correcting

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const Product = () => {
+
+    const params = useParams(); //getting URL 
+
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+        getProduct();
+    });
+
+    const getProduct = () => {
+        fetch(`http://localhost:8080/products/${params.id}`) //using ID path variable from params
+            .then((response) => {
+                return response.json();
+            }).then((data) => {
+                setProduct(data);
+
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
+
     return(
         <div>
             <h1>Product</h1>
